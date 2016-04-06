9bb0caf96e1606f24f0a402f068c918811f4e6cf
judeb <judeb@localhost>
2016-04-06 14:56:57 -0400

    Second Commit

--- TestTwoJLB/src/edu/saintjoe/cs/judeb/TestTwoJLB/TestTwo.java
+++ TestTwoJLB/src/edu/saintjoe/cs/judeb/TestTwoJLB/TestTwo.java
@@ -1,10 +1,33 @@
 package edu.saintjoe.cs.judeb.TestTwoJLB;
 
 public class TestTwo {
+	public class TesTwo {
+		int[] areaCells;
+		int numOfHits = 0;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		public void SetareaCells (int[] area){
+			areaCells = area;
+		}
+	public String checkYourself (String stringGuess) {
+		int guess = Integer.parseInt (stringGuess);
+		String result = "Close, but no cigar";
+		for (int cell : areaCells) {
+			if (guess == cell) {
+				result = "Bingo";
+				numOfHits++;
+				break;
+				
+			}
+		}
 
+		if (numOfHits ==
+			areaCells.length) {
+			result = "You sunk my battleship";
+			}
+		System.out.println(result);
+		return result;
+
+	
 	}
-
-}
+	}	
+	}
